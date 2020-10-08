617c1261a85e4a7c70053937bbba149cb158aa27
Ryan Wilson <rwils83@gmail.com>
2020-10-08 21:28:42 +0100

    Added go

---
+++ go/over-engineered.go
@@ -0,0 +1,17 @@
+// I need to learn more go to make a truly over-engineered version. So for now, this is the best I got
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  var h = "H"
+  var e = "e"
+  var l = "l"
+  var o = "o"
+  var w = "W"
+  var r = "r"
+  var d = "d"
+  fmt.Println(h+e+l+l+o+" "+w+o+r+l+d)
+}
