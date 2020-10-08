2194c44a26d4ecc06dbfc69a80e0d605cb40e02a
Ryan Wilson <rwils83@gmail.com>
2020-10-08 21:45:44 +0100

    Added more languages

---
+++ c++/over-engineered.cpp
@@ -0,0 +1,15 @@
+//You can do better
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string firstWord = "Hello";
+  string space = " ";
+  string secondWord = "World";
+  string sentence = firstWord+space+secondWord;
+  cout << sentence;
+  return 0;
+}
---
+++ javascript/over-engineered.js
@@ -0,0 +1,9 @@
+//This is over-engineered purely due to spinning up an http server
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen(8080);
+console.log('Server Started');
