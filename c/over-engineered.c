ec067dde61e7541c8a6673d27dd8dcf0c12f74e8
Restyled.io <commits@restyled.io>
2021-10-09 04:55:53 +0000

    Restyled by clang-format

--- c/over-engineered.c
+++ c/over-engineered.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-    char firstword[] = "Hello";
-    char space[] = " ";
-    char finalword[] = "World";
-    printf ("%s", firstword);
-    printf ("%s", space);
-    printf ("%s", finalword);
-    return 0;
+int main() {
+  char firstword[] = "Hello";
+  char space[] = " ";
+  char finalword[] = "World";
+  printf("%s", firstword);
+  printf("%s", space);
+  printf("%s", finalword);
+  return 0;
 }
