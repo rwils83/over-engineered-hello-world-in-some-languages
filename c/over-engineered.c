c22878e42f779ab7ef10471c133d7126b72ad089
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-10-09 10:25:09 +0530

    Adding C

---
+++ c/over-engineered.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  char firstword[] = "Hello";
+  char space[] = " ";
+  char finalword[] = "World";
+  printf ("%s", firstword);
+  printf ("%s", space);
+  printf ("%s", finalword);
+  return 0;
+}
