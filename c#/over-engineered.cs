af2c597db26783b3929f299eea22803353746336
Restyled.io <commits@restyled.io>
2021-10-09 22:52:33 +0000

    Restyled by astyle

--- c#/over-engineered.cs
+++ c#/over-engineered.cs
@@ -3,51 +3,53 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    class Program
+class Program
+{
+    async static Task Main(string[] args)
     {
-        async static Task Main(string[] args)
-        {
-            IHelloWorldController helloWorldController = new HelloWorldController();
-            IOutputController outputController = new OutputController();
+        IHelloWorldController helloWorldController = new HelloWorldController();
+        IOutputController outputController = new OutputController();
 
-            string helloWorldString = await helloWorldController.GetHelloWorldStringAsync();
-            outputController.Output(helloWorldString);
-        }
+        string helloWorldString = await helloWorldController.GetHelloWorldStringAsync();
+        outputController.Output(helloWorldString);
+    }
+}
+
+interface IHelloWorldController
+{
+    Task<string> GetHelloWorldStringAsync();
+}
+
+interface IOutputController
+{
+    void Output(string value);
+}
+
+/// <summary>
+/// This class can retrieve the desired "Hello World!" value asynchronously by creating a new Thread via the Task.Run Method.
+/// </summary>
+class HelloWorldController : IHelloWorldController
+{
+    private string getHelloWorld() {
+        return "Hello World!";
     }
 
-    interface IHelloWorldController
+    private Func<string> helloWorldFunc => getHelloWorld;
+
+    public async Task<string> GetHelloWorldStringAsync()
     {
-        Task<string> GetHelloWorldStringAsync();
+        return await Task.Run(helloWorldFunc);
     }
+}
 
-    interface IOutputController
+/// <summary>
+/// This class can output ANY string value you desire.
+/// </summary>
+class OutputController : IOutputController
+{
+    public void Output(string value)
     {
-        void Output(string value);
+        Console.WriteLine(value);
     }
-
-    /// <summary>
-    /// This class can retrieve the desired "Hello World!" value asynchronously by creating a new Thread via the Task.Run Method.
-    /// </summary>
-    class HelloWorldController : IHelloWorldController
-    {
-        private string getHelloWorld() { return "Hello World!"; }
-
-        private Func<string> helloWorldFunc => getHelloWorld;
-
-        public async Task<string> GetHelloWorldStringAsync()
-        {
-            return await Task.Run(helloWorldFunc);
-        }
-    }
-
-    /// <summary>
-    /// This class can output ANY string value you desire.
-    /// </summary>
-    class OutputController : IOutputController
-    {
-        public void Output(string value)
-        {
-            Console.WriteLine(value);
-        }
-    }
+}
 }
