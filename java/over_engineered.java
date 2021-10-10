e0ed5d8f0de25093441daf223707ba5a8f486d05
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-10-10 07:59:11 +0530

    Adding in Java

---
+++ java/over_engineered.java
@@ -0,0 +1,17 @@
+class over_engineered {  
+    public static void main(String args[]) {
+
+        char[] firstWord = {'H', 'e', 'l', 'l', 'o'};
+        char[] space = {' '};
+        char[] finalWord = {'W', 'o', 'r', 'l', 'd'};
+
+        String First = new String (firstWord);
+        String Space = new String (space);
+        String Final = new String (finalWord);
+
+        System.out.print(First);
+        System.out.print(Space);
+        System.out.println(Final);
+        
+    }  
+}  
