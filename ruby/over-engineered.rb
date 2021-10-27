ebea6b71da7455c79d629453b31bf153abd4f466
dticed <lucaspierrealencar@gmail.com>
2021-10-27 08:17:42 -0300

    add ruby hello world
    Signed-off-by: dticed <lucaspierrealencar@gmail.com>

--- ruby/over-engineered.rb
+++ ruby/over-engineered.rb
@@ -1,8 +1,8 @@
-my_hello_world_array = ['H','e','l','l','o','W','o','r','l','d']
+my_hello_world_array = ['H','e','l','l','o',' W','o','r','l','d']
 my_hello_world_string = ''
 
 for letters in my_hello_world_array
   my_hello_world_string = my_hello_world_string + letters
 end
 
-puts 'Hello, world!'
+puts my_hello_world_string
