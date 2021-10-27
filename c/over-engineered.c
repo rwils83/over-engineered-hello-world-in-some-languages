8d6c55975e4f2d7098c1f4e169e44ddfc97414ee
Restyled.io <commits@restyled.io>
2021-10-27 11:33:35 +0000

    Restyled by astyle

--- c/over-engineered.c
+++ c/over-engineered.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  char firstword[] = "Hello";
-  char space[] = " ";
-  char finalword[] = "World";
-  printf("%s", firstword);
-  printf("%s", space);
-  printf("%s", finalword);
-  return 0;
+    char firstword[] = "Hello";
+    char space[] = " ";
+    char finalword[] = "World";
+    printf("%s", firstword);
+    printf("%s", space);
+    printf("%s", finalword);
+    return 0;
 }
--- c/simple_example.c
+++ c/simple_example.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  char text[] = "Hello World";
-  printf("%s", text);
-  return 0;
+    char text[] = "Hello World";
+    printf("%s", text);
+    return 0;
 }
